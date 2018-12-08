fcf0d87314d65c4ea2acf09e4f97466a1d74ede9
Costi Rada <costi.rada@hotmail.com>
2018-12-08 20:06:21 +0200

    Improved algorithm

--- Day7.js
+++ Day7.js
@@ -6,78 +6,83 @@ var day7Input = rawDay7Input.toString().split('\r\n');
 
 var order = [];
 var pre = [];
-for (i = 0; i < day7Input.length; i++) {
-    let line = day7Input[i].split(' ');
-    let first = line[1];
-    let second = line[7];
+for (let i = 0; i < day7Input.length; i++) {
+  let line = day7Input[i].split(' ');
+  let first = line[1];
+  let second = line[7];
 
-    //console.log(first  + "<" + second);
+  //console.log(first  + "<" + second);
 
-    if (order[first] === undefined) 
-    {
-        order[first] = 0;
-        pre[first] = "";
-    }
+  if (order[first] === undefined) {
+    order[first] = 0;
+    pre[first] = "";
+  }
 
-    if (order[second] === undefined) 
-    {
-        order[second] = order[first] + 1;
-        pre[second] = first;
+  if (order[second] === undefined) {
+    order[second] = order[first] + 1;
+    pre[second] = first;
+  }
+  else {
+    let newOrder = order[first] + 1;
+    if (newOrder > order[second]) {
+      order[second] = newOrder;
+      for (let key in pre) {
+        if (pre[key].indexOf(second) != -1)
+          order[key]++;
+      }
     }
-    else 
-    {
-        let newOrder = order[first] + 1;
-        if (newOrder > order[second])
-          order[second] = newOrder;
-        pre[second] += first;  
-    }
+    pre[second] += first;
+  }
 }
 
 var gg = [];
 for (var key in order) {
-    gg.push({ id: key, order: order[key], p: pre[key] });
+  gg.push({ id: key, order: order[key], p: pre[key] });
 }
 
-gg.sort(function (aNode1, aNode2) { 
-    if (aNode1.order == aNode2.order) 
-    {
-      if (aNode1.id < aNode2.id)
-        return -1;
-      else if (aNode1.id == aNode2.id)
-        return 0;
-      else 
-        return 1;
-    }
+gg.sort(function (aNode1, aNode2) {
+  if (aNode1.order == aNode2.order) {
+    if (aNode1.id < aNode2.id)
+      return -1;
+    else if (aNode1.id == aNode2.id)
+      return 0;
     else
-      return (aNode1.order < aNode2.order) ? -1 : 1;
+      return 1;
+  }
+  else
+    return (aNode1.order < aNode2.order) ? -1 : 1;
 });
 
+console.log(gg);
+
 var pp = [];
-for (i = 0; i < gg.length; i++)
-{
-  for (j = 0; j < gg.length; j++)
-    if (gg[j].order == i)
-    {
-      if (pp[i] === undefined)
-        pp[i] = "";
-      pp[i] += gg[j].id;
-    }
+let iOrder = -1;
+let group = "";
+for (let i = 0; i < gg.length; i++) {
+  if (gg[i].order == iOrder)
+    group += gg[i].id;
+  else {
+    if (group.length > 0)
+      pp.push(group);
+    iOrder = gg[i].order;
+    group = gg[i].id;
+  }
 }
 
-function NoDeps(aResult, aDeps) 
-{
+if (group.length > 0)
+  pp.push(group);
+
+function NoDeps(aResult, aDeps) {
   for (let k = 0; k < aDeps.length; k++)
     if (aResult.indexOf(aDeps[k]) == -1)
       return false;
-  return true;      
+  return true;
 }
 
 var result = "";
 var queue = [];
-for (i = 0; i < pp.length; i++) 
-{
-  if (queue.length > 0) 
-  {
+for (i = 0; i < pp.length; i++) {
+  if (queue.length > 0) {
     let hhh = pp[i];
     let ccc = queue.concat(hhh.split(''));
     ccc.sort();
@@ -86,10 +91,8 @@ for (i = 0; i < pp.length; i++)
 
     let newQueue = [];
     let found = false;
-    for (j = 0; j < ccc.length; j++) 
-    {
-      if (!found && NoDeps(result, pre[ccc[j]])) 
-      {
+    for (j = 0; j < ccc.length; j++) {
+      if (!found && NoDeps(result, pre[ccc[j]])) {
         result += ccc[j]
         found = true;
       }
@@ -99,29 +102,25 @@ for (i = 0; i < pp.length; i++)
 
     queue = newQueue;
   }
-  else
-  {
+  else {
     result += pp[i][0];
-    queue = queue.concat(pp[i].substr(1).split(''));    
+    queue = queue.concat(pp[i].substr(1).split(''));
   }
 }
-  
-while(queue.length > 0)
-{
-    let newQueue = [];
-    let found = false;
-    for (j = 0; j < queue.length; j++) 
-    {
-      if (!found && NoDeps(result, pre[queue[j]])) 
-      {
-        result += queue[j]
-        found = true;
-      }
-      else
-        newQueue.push(queue[j]);
-    }
 
-    queue = newQueue;
+while (queue.length > 0) {
+  let newQueue = [];
+  let found = false;
+  for (j = 0; j < queue.length; j++) {
+    if (!found && NoDeps(result, pre[queue[j]])) {
+      result += queue[j]
+      found = true;
+    }
+    else
+      newQueue.push(queue[j]);
+  }
+
+  queue = newQueue;
 }
 
 pre.sort();
@@ -130,27 +129,23 @@ console.log(gg);
 console.log(JSON.stringify(pp));
 console.log(result);
 
-function GetBestWorker(aWorkers, aCount) 
-{
-  let minLoad = 100000;  
+function GetBestWorker(aWorkers, aCount) {
+  let minLoad = 100000;
   let index = 0;
   for (let w = 0; w < aCount; w++)
-    if (aWorkers[w] == undefined) 
-    {
+    if (aWorkers[w] == undefined) {
       aWorkers[w] = [];
       return w;
     }
     else
-      if (aWorkers[w].length < minLoad) 
-      {
+      if (aWorkers[w].length < minLoad) {
         minLoad = aWorkers[w].length;
-        index = w;    
+        index = w;
       }
-  return index;      
+  return index;
 }
 
-function NormalizeWorkers(aWorkers, aCount, aWorkCount)
-{
+function NormalizeWorkers(aWorkers, aCount, aWorkCount, ) {
   if (aWorkCount >= aCount)
     return;
 
@@ -158,13 +153,11 @@ function NormalizeWorkers(aWorkers, aCount, aWorkCount)
   for (let i = 0; i < aCount; i++)
     if ((aWorkers[i] !== undefined) && (aWorkers[i].length < min))
       min = aWorkers[i].length;
-  
-  for (let i = 0; i < aCount; i++) 
-  {
+
+  for (let i = 0; i < aCount; i++) {
     if (aWorkers[i] === undefined)
       aWorkers[i] = [];
-    if (aWorkers[i].length < min) 
-    {
+    if (aWorkers[i].length < min) {
       let toAdd = min - aWorkers[i].length;
       for (let j = 0; j < toAdd; j++)
         aWorkers[i].push('.');
@@ -172,38 +165,60 @@ function NormalizeWorkers(aWorkers, aCount, aWorkCount)
   }
 }
 
-function PrintWorkers(aWorkers) 
-{
-  for (let i = 0; i < aWorkers.length; i++)
-  {
-    let line = "";
-    for (let j = 0; j < aWorkers[i].length; j++)
-      line += aWorkers[i][j];
+function MaximizeWorkers(aWorkers, aCount) {
+  let max = 0;
+  //for (let i = 0; i < aCount; i++)
+  // if (aWorkers[i].length > max)
+  //   max = aWorkers[i].length;
+
+  max = aWorkers.reduce(function (max, aValue) {
+    if (aValue.length > max)
+      max = aValue.length;
+    return max;
+  }, 0);
+
+  for (let i = 0; i < aCount; i++) {
+    if (aWorkers[i].length < max) {
+      let toAdd = max - aWorkers[i].length;
+      for (let j = 0; j < toAdd; j++)
+        aWorkers[i].push('.');
+    }
+  }
+}
+
+function PrintWorkers(aWorkers) {
+  let secondCount = aWorkers[0].length;
+  for (let j = 0; j < secondCount; j++) {
+    let second = (j < 10) ? "0" : "";
+    second += j.toString();
+    let line = second + " ";
+    for (let i = 0; i < aWorkers.length; i++)
+      line += "   " + aWorkers[i][j];
     console.log(line);
   }
 }
 
 const workerCount = 5;
 var workers = [];
-for (let i = 0; i < pp.length; i++) 
-{
+for (let i = 0; i < pp.length; i++) {
   let max = 0;
   let workerIndex = 0;
-  for (let j = 0; j < pp[i].length; j++) 
-  {
+  for (let j = 0; j < pp[i].length; j++) {
     let step = pp[i].charCodeAt(j) - 65 + 1
 
     if (step > max)
       max = step;
 
-    workerIndex = GetBestWorker(workers, workerCount); 
-    for (let k = 0; k < step; k++)  
-      workers[workerIndex].push(pp[i][j]);    
+    workerIndex = GetBestWorker(workers, workerCount);
+    for (let k = 0; k < step; k++)
+      workers[workerIndex].push(pp[i][j]);
   }
 
   //if (i < pp.length - 1)
-    NormalizeWorkers(workers, workerCount, pp[i].length);
+  NormalizeWorkers(workers, workerCount, pp[i].length);
 }
+
+MaximizeWorkers(workers, workerCount);
 
 PrintWorkers(workers);
 
@@ -211,6 +226,5 @@ var seconds = 0;
 for (let i = 0; i < workers.length; i++)
   if (workers[i].length > seconds)
     seconds = workers[i].length;
-  
+
 console.log(seconds + 1);
-  
